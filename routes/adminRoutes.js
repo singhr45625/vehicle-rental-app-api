9f08469c99802eca7d7267a7de106b3070a0398d
rishu2004-rs <kpuja0969@gmail.com>
2026-01-06 12:48:57 +0530

    c

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { getPendingUsers, approveUser, rejectUser, createUser, updateUser } = require('../controllers/adminController');
+const {
+    getAllUsers,
+    getPendingUsers,
+    approveUser,
+    rejectUser,
+    createUser,
+    updateUser
+} = require('../controllers/adminController');
 const adminCheck = require('../middleware/admin');
 const { authenticateUser } = require('../middleware/auth');
 
 // All routes require authentication and admin role
 router.use(authenticateUser, adminCheck);
 
+// Routes
 router.get('/pending', getPendingUsers);
+router.get('/users', getAllUsers);
 router.put('/approve/:userId', approveUser);
 router.put('/reject/:userId', rejectUser);
 router.post('/create', createUser);
