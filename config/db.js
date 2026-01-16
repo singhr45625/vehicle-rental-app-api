1987e205fbe9a60d096d797e4133a71134702458
rishu2004-rs <kpuja0969@gmail.com>
2026-01-16 22:09:06 +0530

    fixed db

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI || process.env.MONGODB_URI, {
       serverSelectionTimeoutMS: 5000,
     });
     console.log(`MongoDB Atlas Connected: ${conn.connection.host}`);
